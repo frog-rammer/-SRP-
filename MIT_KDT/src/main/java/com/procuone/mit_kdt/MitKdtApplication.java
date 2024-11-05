12a305eeea86a11fe67f09fa81fbed525bdb0570
SeungHo KIM <wpk1008@naver.com>
2024-11-05 19:19:25 +0900

    MIT_KDT 프로젝트 생성
    김승호
    
    2024-11-05

---
+++ MIT_KDT/src/main/java/com/procuone/mit_kdt/MitKdtApplication.java
@@ -0,0 +1,13 @@
+package com.procuone.mit_kdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitKdtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MitKdtApplication.class, args);
+    }
+
+}
---
+++ MIT_KDT/src/test/java/com/procuone/mit_kdt/MitKdtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procuone.mit_kdt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitKdtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
