fbdc8a893c9f6baa171fe8c03751902ea39c16d6
enrkfka <enrkfka@naver.com>
2024-11-11 19:07:10 +0900

    14차 커밋
    한가람
    
    productdto, 레포지토리, 서비스, impl, 컨트롤러 추가
    
    24-11-11

--- MIT_KDT/src/main/java/com/procuone/mit_kdt/dto/ProductDTO.java
+++ MIT_KDT/src/main/java/com/procuone/mit_kdt/dto/ProductDTO.java
@@ -1,4 +1,21 @@
 package com.procuone.mit_kdt.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
+
+    private String productId; // 제품코드
+    private String productName; // 품목명
+    private String productDimension; // 규격
+    private String productLargeCategory; // 대분류
+    private String productMediumCategory; // 중분류
+    private String productSmallCategory; // 소분류
+    private String productMaterial; // 재질
+    private String productDrawingFile; // 도면파일
+    private String productQuantity; // 수량
 }
