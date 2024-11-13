8f917794f3e2e6fe57d92168d2d5dcd59fe95aa3
SeungHo KIM <wpk1008@naver.com>
2024-11-13 15:05:10 +0900

    커커커커밋
    커커커커밋
    김승호
    2024-11-13

--- MIT_KDT/src/main/java/com/procuone/mit_kdt/dto/ItemDTOs/ItemDTO.java
+++ MIT_KDT/src/main/java/com/procuone/mit_kdt/dto/ItemDTOs/ItemDTO.java
@@ -15,6 +15,5 @@ public class ItemDTO {
     private String itemName; // 품목명
     private String drawingFile; // 도면 파일 경로
     private String dimensions; // 치수 정보
-
     private Long categoryId; // 카테고리 ID 참조
 }
