a173091d50342044095bc632ec78b76c296c3931
Jinhyuck0415 <eon6407@naver.com>
2024-11-14 15:35:24 +0900

    커밋
    장진혁
    
    css수정
    2024-11-14

--- MIT_KDT/src/main/java/com/procuone/mit_kdt/controller/ContractController.java
+++ MIT_KDT/src/main/java/com/procuone/mit_kdt/controller/ContractController.java
@@ -3,10 +3,13 @@ package com.procuone.mit_kdt.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/contract")
 public class ContractController {
+
+
     @GetMapping("/register")
     public String register() {
         return "procurementPlan/compareContracts";
@@ -14,5 +17,9 @@ public class ContractController {
     @GetMapping("/registerContract")
     public String registerContract() {
         return "procurementPlan/registerContract";
+    }
+    @GetMapping("/registerContract2")
+    public String registerContract2(RedirectAttributes redirectAttributes){
+        return "redirect:/contract/register";
     }
 }
