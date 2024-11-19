7ad6720a90b2af86c56dbc627d3cffe97a8d88b4
Jinhyuck0415 <eon6407@naver.com>
2024-11-19 12:44:11 +0900

    커밋
    장진혁
    
    로그인 문제 수정
    
    2024-11-19

--- MIT_KDT/src/main/java/com/procuone/mit_kdt/repository/MemberRepository.java
+++ MIT_KDT/src/main/java/com/procuone/mit_kdt/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.procuone.mit_kdt.repository;
 
 import com.procuone.mit_kdt.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -15,5 +17,7 @@ public interface MemberRepository extends JpaRepository<Member, String> {
     // 회원ID가 존재하는지 확인
     boolean existsByMemberId(String memberId);
 
-    String findUserTypeByMemberId(String memberId);
+    // UserType을 String으로 반환하도록 수정
+    @Query("SELECT m.type FROM Member m WHERE m.memberId = :memberId")
+    String findUserTypeByMemberId(@Param("memberId") String memberId);
 }
