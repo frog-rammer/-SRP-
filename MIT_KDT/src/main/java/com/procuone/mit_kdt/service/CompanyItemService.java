028ff613d53f9979849855fb57935025992a34c5
SeungHo KIM <wpk1008@naver.com>
2024-11-20 14:57:02 +0900

    commit
    회사에서
    공급 물품 등록 완료
    김승호
    2024-11-20

--- MIT_KDT/src/main/java/com/procuone/mit_kdt/service/CompanyItemService.java
+++ MIT_KDT/src/main/java/com/procuone/mit_kdt/service/CompanyItemService.java
@@ -2,6 +2,8 @@ package com.procuone.mit_kdt.service;
 
 import com.procuone.mit_kdt.dto.CompanyItemDTO;
 
+import java.util.List;
+
 public interface CompanyItemService {
-    void saveCompanyItem(CompanyItemDTO companyItemDTO);
+    void saveCompanyItems(List<CompanyItemDTO> companyItemDTOs);
 }
