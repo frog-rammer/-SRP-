4b7a07190df12db4bd00648eb924f2260441920b
enrkfka <enrkfka@naver.com>
2024-11-21 16:58:01 +0900

    00

--- MIT_KDT/src/main/java/com/procuone/mit_kdt/service/CompanyItemService.java
+++ MIT_KDT/src/main/java/com/procuone/mit_kdt/service/CompanyItemService.java
@@ -8,4 +8,5 @@ public interface CompanyItemService {
     void saveCompanyItems(List<CompanyItemDTO> companyItemDTOs);
     // 품목 ID로 공급업체 리스트를 조회하는 메서드 선언
     List<CompanyItemDTO> getSuppliersByItem(Long itemId);
+    CompanyItemDTO getCompanyItemByBussinessId(String bussinessId);
 }
