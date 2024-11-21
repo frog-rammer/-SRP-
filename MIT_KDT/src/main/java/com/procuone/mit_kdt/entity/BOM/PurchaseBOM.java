e9659d654ef2a02681c6b227f20fe66bc333c8f1
SeungHo KIM <wpk1008@naver.com>
2024-11-21 15:20:34 +0900

    commit
    commit 뭐한지 모르겠다
    
    김승호
    
    2024-11-21

---
+++ MIT_KDT/src/main/java/com/procuone/mit_kdt/entity/BOM/PurchaseBOM.java
@@ -0,0 +1,30 @@
+package com.procuone.mit_kdt.entity.BOM;
+
+import com.procuone.mit_kdt.entity.Company;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PurchaseBOM {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_code", nullable = false)
+    private String productCode;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id", referencedColumnName = "id", nullable = false)
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "business_id", referencedColumnName = "businessId", nullable = false)
+    private Company company;
+
+    private Integer quantity;
+}
